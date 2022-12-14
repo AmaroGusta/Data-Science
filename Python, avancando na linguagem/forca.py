2911028e63f8e59e496739175094a227dabb330b
Gustavo Amaro <69257054+AmaroGusta@users.noreply.github.com>
2022-12-13 22:58:33 -0300

    hangman game, and dealing with list

---
+++ Python, avancando na linguagem/forca.py
@@ -0,0 +1,34 @@
+palavra_secreta = 'comida'.upper()
+palavra = []
+for i in range(len(palavra_secreta)):
+    palavra.append('')
+
+enforcou = False
+acertou = False
+
+print('୮' + '_' * len(palavra_secreta))
+
+while not acertou and not enforcou:
+    chute = input('Chute uma letra: ').upper().strip()
+    index = 0
+    for letra in palavra_secreta:
+        if letra == chute:
+            print(f'Tem {letra} na posição {index}')
+            palavra[index] = letra
+            print(palavra)
+
+        index += 1
+
+    if palavra_secreta.find(chute) == -1:
+        print('Não há essa letra na palavra')
+
+    if palavra[0, len(palavra)] == palavra_secreta:
+        acertou = True
+
+palavra_completa = ''
+for i in range(len(palavra_secreta)):
+    palavra_completa += palavra[i]
+
+print(f'A palavra era {palavra_completa}')
+
+print('Fim de jogo.')
