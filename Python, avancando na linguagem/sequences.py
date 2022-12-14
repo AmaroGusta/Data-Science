5106688d79a601a5ffbbd796cfee356bd773e2f8
Gustavo Amaro <69257054+AmaroGusta@users.noreply.github.com>
2022-12-14 20:01:21 -0300

    sequences

---
+++ Python, avancando na linguagem/sequences.py
@@ -0,0 +1,12 @@
+# tuples -> immutable
+tuple_type = ('name', 5)
+
+# lists -> mutable
+list_type = ['name', 6]
+
+# set -> allows only unique elements
+set_type = {'name'}  # doesn't have a index
+set_type.add(7)  # doesn't use append, but add
+
+# dictionary
+dictionary_type = {'name': 'unknown', 'age': 8}
