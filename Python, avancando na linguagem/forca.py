625d0399536c3e86d6d04c3620dc2bc36b250f65
Gustavo Amaro <69257054+AmaroGusta@users.noreply.github.com>
2022-12-15 22:11:42 -0300

    archives and counting error on hangman game

--- Python, avancando na linguagem/forca.py
+++ Python, avancando na linguagem/forca.py
@@ -1,12 +1,26 @@
-palavra_secreta = 'comida'.upper()
+from random import randint
+
+arquivo = open('palavras.txt', 'r')
+palavras = []
+
+for linha in arquivo:
+    linha = linha.strip()
+    palavras.append(linha)
+
+palavra_secreta = palavras[randint(0, len(palavras))].upper()
 palavra = []
 for i in range(len(palavra_secreta)):
     palavra.append('')
 
+# teacher used
+# palavra = ['_' for letra in palavra_secreta]
+# and it worked there, but got a error here
+
 enforcou = False
 acertou = False
+erros = 0
 
-print('୮' + '_' * len(palavra_secreta))
+print('୮' + '_ ' * len(palavra_secreta))
 
 while not acertou and not enforcou:
     chute = input('Chute uma letra: ').upper().strip()
@@ -21,6 +35,9 @@ while not acertou and not enforcou:
 
     if palavra_secreta.find(chute) == -1:
         print('Não há essa letra na palavra')
+        erros += 1
+        if erros == 6:
+            enforcou = True
 
     acertando = 0
     for i in range(len(palavra)):
@@ -33,6 +50,6 @@ palavra_completa = ''
 for i in range(len(palavra_secreta)):
     palavra_completa += palavra[i]
 
-print(f'A palavra era {palavra_completa}')
+print(f'A palavra era {palavra_secreta}\n {erros} erros')
 
 print('Fim de jogo.')
