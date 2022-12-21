b0551591281230cb7196ee56d45ed264ae17121d
Gustavo Amaro <69257054+AmaroGusta@users.noreply.github.com>
2022-12-20 21:07:57 -0300

    hangman game finished

--- Python, avancando na linguagem/forca.py
+++ Python, avancando na linguagem/forca.py
@@ -1,13 +1,73 @@
 from random import randint
 
-arquivo = open('palavras.txt', 'r')
-palavras = []
+# turns palavras.txt as default document, but allows using others
+def definir_palavra_secreta(arquivo='palavras.txt'):
+    arquivo = open(arquivo, 'r')
+    palavras = []
 
-for linha in arquivo:
-    linha = linha.strip()
-    palavras.append(linha)
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
 
-palavra_secreta = palavras[randint(0, len(palavras))].upper()
+    arquivo.close()
+
+    palavra_secreta = palavras[randint(0, len(palavras))].upper()
+    return palavra_secreta
+
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+
+palavra_secreta = definir_palavra_secreta()
+
 palavra = []
 for i in range(len(palavra_secreta)):
     palavra.append('')
@@ -31,13 +91,16 @@ while not acertou and not enforcou:
             palavra[index] = letra
             print(palavra)
 
+
         index += 1
 
     if palavra_secreta.find(chute) == -1:
         print('Não há essa letra na palavra')
         erros += 1
-        if erros == 6:
+        desenha_forca(erros)
+        if erros == 7:
             enforcou = True
+            print('Você foi enforcado! (X_X)')
 
     acertando = 0
     for i in range(len(palavra)):
@@ -50,6 +113,6 @@ palavra_completa = ''
 for i in range(len(palavra_secreta)):
     palavra_completa += palavra[i]
 
-print(f'A palavra era {palavra_secreta}\n {erros} erros')
+print(f'A palavra era {palavra_secreta}')
 
 print('Fim de jogo.')
