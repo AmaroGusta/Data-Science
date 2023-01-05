94a81a456b4aa91735cdee4bb4de326cfa5dd70d
Gustavo Amaro <69257054+AmaroGusta@users.noreply.github.com>
2023-01-05 19:53:39 -0300

    course concluded

--- Python, entendendo orientacao a objetos/conta.py
+++ Python, entendendo orientacao a objetos/conta.py
@@ -1,4 +1,6 @@
 class Conta:
+    cd_bancos = {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
+
     def __init__(self, numero, titular, saldo, limite):
         # __init__ --> constructor, lets the class initialize the
         # object's attributes
@@ -14,17 +16,25 @@ class Conta:
     def deposito(self, valor_deposito):
         self.__saldo += valor_deposito
 
+    def __autorizacao(self, valor_a_sacar):
+        valor_disponivel_saque = self.__limite + self.__saldo
+        return valor_disponivel_saque > valor_a_sacar
+    # private method
+
     def saque(self, valor_saque):
-        autorizacao = self.__saldo + valor_saque >= self.__limite
-        if autorizacao:
+        # autorizacao = valor_saque < self.__saldo + self.__limite
+        if self.__autorizacao(valor_saque):
+            self.__saldo -= valor_saque
+        else:
             print('Saque ultrapassa o limite da conta de: R$'
                   f' {self.__limite}')
-        else:
-            self.__saldo -= valor_saque
 
     def transferencia(self, destino, valor):
-        self.saque(valor)
-        destino.deposito(valor)
+        if self.__autorizacao(valor):
+            self.saque(valor)
+            destino.deposito(valor)
+        else:
+            print('Transferência não autorizada.')
 
     def dados(self):
         dados = {'numero': self.__numero, 'titular': self.__titular,
@@ -35,7 +45,7 @@ class Conta:
     def get_numero(self):
         return self.__numero
 
-    """ OR:
+    """ OR (fancier):
     @property
     def numero(self):
         return self.__numero
@@ -52,4 +62,11 @@ class Conta:
     """def set_limite(self, novo_limite):
         self.__limite = novo_limite
     """
-# this process of privating the attributes is called encapsulation
+
+    @staticmethod
+    # it's the class' properties, since doesn't need an object to
+    # be called: Conta.codigos_bancos() -> works
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
+    # this could also be done by creating an attribute just after
+    # creating the class itself, as in line 2
